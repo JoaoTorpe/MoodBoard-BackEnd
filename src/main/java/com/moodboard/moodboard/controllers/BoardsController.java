5fe214ea40a14bbc9a7cc36a2b73aab10ca99b02
JoaoTorpe <victotorpe10@gmail.com>
2023-07-19 15:35:55 -0300

    Added a delete method in BoardsController

--- src/main/java/com/moodboard/moodboard/controllers/BoardsController.java
+++ src/main/java/com/moodboard/moodboard/controllers/BoardsController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,12 @@ public class BoardsController {
 		  Repository.save(b);
 		  
 	  }
+	  
+	@DeleteMapping(value = "/{id}")
+	public void deleteBoard(@PathVariable Long id ) {
+		
+		Repository.deleteById(id);
+	}
 	  
 	  
 	
